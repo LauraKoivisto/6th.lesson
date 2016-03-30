9bd52e84e74a1a01ac1c34ebf56df72703523010
laukoi <laukoi@greeny.cs.tlu.ee>
2016-03-31 00:50:08 +0300

    made some homework

--- app.php
+++ app.php
@@ -1,103 +1,167 @@
-<?php
-	// require another php file
-	// ../../../ => 3 folders back
-	require_once("../config.php");
-	$everything_was_okay = true;
-	//*********************
-	// TO field validation
-	//*********************
-	if(isset($_GET["to"])){ //if there is ?to= in the URL
-		if(empty($_GET["to"])){ //if it is empty
-			$everything_was_okay = false; //empty
-			echo "Please enter the recipient! <br>"; // yes it is empty
-		}else{
-			echo "To: ".$_GET["to"]."<br>"; //no it is not empty
-		}
-	}else{
-		$everything_was_okay = false; // do not exist
-	}
-	//check if there is variable in the URL
-	if(isset($_GET["message"])){
-
-		//only if there is message in the URL
-		//echo "there is message";
-
-		//if its empty
-		if(empty($_GET["message"])){
-			//it is empty
-			$everything_was_okay = false;
-			echo "Please enter the message! <br>";
-		}else{
-			//its not empty
-			echo "Message: ".$_GET["message"]."<br>";
-		}
-
-	}else{
-		//echo "there is no such thing as message";
-		$everything_was_okay = false;
-	}
 
 
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#">Brand</a>
+	</div>
 
-	/***********************
-	**** SAVE TO DB ********
-	***********************/
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-	// ? was everything okay
-	if($everything_was_okay == true){
+		<ul class="nav navbar-nav">
 
-		echo "Saving to database ... ";
+		<li class="active">
+			<a href="app.php">
 
+				Make your list
 
-		//connection with username and password
-		//access username from config
-		//echo $db_username;
+			</a>
+		</li>
 
-		// 1 servername
-		// 2 username
-		// 3 password
-		// 4 database
-		$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_laukoi");
+		<li>
+			<a href="table.php">
+				Your list
+			</a>
+		</li>
 
-		$stmt = $mysql->prepare("INSERT INTO messages_sample (recipient, message) VALUES (?,?)");
+		</ul>
 
-		//echo error
-		echo $mysql->error;
+	</div><!-- /.navbar-collapse -->
+	</div><!-- /.container-fluid -->
+</nav>
 
-		// we are replacing question marks with values
-		// s - string, date or smth that is based on characters and numbers
-		// i - integer, number
-		// d - decimal, float
-
-		//for each question mark its type with one letter
-		$stmt->bind_param("ss", $_GET["to"], $_GET["message"]);
-
-		//save
-		if($stmt->execute()){
-			echo "saved sucessfully";
-		}else{
-			echo $stmt->error;
-		}
-
-
-	}
+<div class="container">
 
 
-?>
-<br>
-<a href="table.php">table</a>
-<h2> First application </h2>
-
-<form method="get">
-	<label for="to">to:* <label>
-	<input type="text" name="to"><br><br>
-
-	<label for="message">Message:* <label>
-	<input type="text" name="message"><br><br>
-
-	<!-- This is the save button-->
-	<input type="submit" value="Save to DB">
+<h2 class="text-center">Grocery List</h2>
+<h3 class="text-center">What you need to buy</h3>
 
 <form>
+	<div class="row">
+		<div class="col-md-3 col-sm-6">
+			<div class="form-group">
+<form method="get">
+	<label for="buy">Buy:* <label>
+	<input type="text" name="buy"><br><br>
+</div>
+</div>
+</div>
 
-<p>Idea</p>
+
+
+<div class="row">
+<div class="col-md-3 col-sm-6">
+		<div class="form-group">
+	<label for="budget">Budget: <label>
+	<input type="text" name="budget"><br><br>
+</div>
+</div>
+</div>
+
+	<!-- This is the save button-->
+
+<h4>Press the heart and add it to the list</h4>
+<button type="submit" class="btn btn-danger btn-circle btn-xl"><i class="glyphicon glyphicon-heart"></i></button>
+
+
+	<br><br>
+
+	</html>
+
+	<?php require_once("header.php"); ?>
+	<?php
+		// require another php file
+		// ../../../ => 3 folders back
+		require_once("../config.php");
+		$everything_was_okay = true;
+		//*********************
+		// TO field validation
+		//*********************
+		if(isset($_GET["buy"])){ //if there is ?to= in the URL
+			if(empty($_GET["buy"])){ //if it is empty
+				$everything_was_okay = false; //empty
+				echo "Please enter the item <br>"; // yes it is empty
+			}else{
+				echo "Buy: ".$_GET["buy"]."<br>"; //no it is not empty
+			}
+		}else{
+			$everything_was_okay = false; // do not exist
+		}
+		//check if there is variable in the URL
+		if(isset($_GET["budget"])){
+
+			//only if there is message in the URL
+			//echo "there is message";
+
+			//if its empty
+			if(empty($_GET["budget"])){
+				//it is empty
+				$everything_was_okay = false;
+				echo "Please enter the budget <br>";
+			}else{
+				//its not empty
+				echo "Budget: ".$_GET["budget"]."<br>";
+			}
+
+		}else{
+			//echo "there is no such thing as message";
+			$everything_was_okay = false;
+		}
+
+
+
+		/***********************
+		**** SAVE TO DB ********
+		***********************/
+
+		// ? was everything okay
+		if($everything_was_okay == true){
+
+			echo "Added to list ";
+
+
+			//connection with username and password
+			//access username from config
+			//echo $db_username;
+
+			// 1 servername
+			// 2 username
+			// 3 password
+			// 4 database
+			$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_laukoi");
+
+			$stmt = $mysql->prepare("INSERT INTO Grocery_list (item_1, budget) VALUES (?,?)");
+
+			//echo error
+			echo $mysql->error;
+
+			// we are replacing question marks with values
+			// s - string, date or smth that is based on characters and numbers
+			// i - integer, number
+			// d - decimal, float
+
+			//for each question mark its type with one letter
+			$stmt->bind_param("ss", $_GET["buy"], $_GET["budget"]);
+
+			//save
+
+
+			if($stmt->execute()){
+				echo "saved sucessfully";
+			}else{
+				echo $stmt->error;
+			}
+
+
+		}
+
+
+	?>
--- table.php
+++ table.php
@@ -1,3 +1,4 @@
+<?php require_once("header.php"); ?>
 <?php
 	// table.php
 
@@ -16,7 +17,7 @@
 		echo "Deleting row with id:".$_GET["delete"];
 
 		// NOW() = current date-time
-		$stmt = $mysql->prepare("UPDATE messages_sample SET deleted=NOW() WHERE id = ?");
+		$stmt = $mysql->prepare("UPDATE Grocery_list SET deleted=NOW() WHERE id = ?");
 
 		echo $mysql->error;
 
@@ -40,7 +41,7 @@
 
 
 	//SQL sentence
-	$stmt = $mysql->prepare("SELECT id, recipient, message, created FROM messages_sample WHERE deleted IS NULL ORDER BY created DESC LIMIT 10");
+	$stmt = $mysql->prepare("SELECT id, item_1, budget FROM Grocery_list WHERE deleted IS NULL ORDER BY created DESC LIMIT 10");
 
 	//WHERE deleted IS NULL show only those that are not deleted
 
@@ -48,7 +49,7 @@
 	echo $mysql->error;
 
 	//variables for data for each row we will get
-	$stmt->bind_result($id, $recipient, $message, $created);
+	$stmt->bind_result($id, $item_1, $budget);
 
 	//query
 	$stmt->execute();
@@ -56,12 +57,11 @@
 	$table_html = "";
 
 	//add smth to string .=
-	$table_html .= "<table>";
+	$table_html .= "<table class=table table-'>";
 		$table_html .= "<tr>";
 			$table_html .= "<th>ID</th>";
-			$table_html .= "<th>Recipient</th>";
-			$table_html .= "<th>Message</th>";
-			$table_html .= "<th>Created</th>";
+			$table_html .= "<th>Your Item</th>";
+			$table_html .= "<th>How much</th>";
 			$table_html .= "<th>Delete ?</th>";
 		$table_html .= "</tr>";
 
@@ -73,19 +73,169 @@
 		//echo $id." ".$message."<br>";
 		$table_html .= "<tr>"; //start new row
 			$table_html .= "<td>".$id."</td>"; //add columns
-			$table_html .= "<td>".$recipient."</td>";
-			$table_html .= "<td>".$message."</td>";
-			$table_html .= "<td>".$created."</td>";
+			$table_html .= "<td>".$item_1."</td>";
+			$table_html .= "<td>".$budget."</td>";
 			$table_html .= "<td>
-								<a href='?delete=".$id."'>X</a>
+			<a class='btn btn-danger'
+								href='?delete=".$id."'>Away!</a>
 							</td>";
+
+
 
 		$table_html .= "</tr>"; //end row
 	}
 	$table_html .= "</table>";
-	echo $table_html;
+
 
 
 
 ?>
-<a href="app.php">app</a>
+
+
+
+		<nav class="navbar navbar-inverse">
+	  <div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		  </button>
+		  <a class="navbar-brand" href="#">Grocery list</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+		  <ul class="nav navbar-nav">
+
+			<li>
+				<a href="app.php">
+					Forgot something?
+				</a>
+			</li>
+
+
+			<li class="active" >
+				<a href="table.php">
+					List
+				</a>
+			</li>
+
+		  </ul>
+
+		</div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+
+	<div class="container">
+
+		<h1> This is what you need to buy </h1>
+
+	<?php echo $table_html; ?>
+
+	</div>
+
+
+
+
+
+
+
+  </body>
+ </html>
+
+ <?php require_once("header.php"); ?>
+ <?php
+ 	// table.php
+
+ 	//getting our config
+ 	require_once("../config.php");
+
+ 	//create connection
+ 	$mysql = new mysqli("localhost", $db_username, $db_password, "webpr2016_laukoi");
+
+
+ 	/*
+ 		IF THERE IS ?DELETE=ROW_ID in the url
+ 	*/
+ 	if(isset($_GET["delete"])){
+
+ 		echo "Deleting row with id:".$_GET["delete"];
+
+ 		// NOW() = current date-time
+ 		$stmt = $mysql->prepare("UPDATE Grocery_list SET deleted=NOW() WHERE id = ?");
+
+ 		echo $mysql->error;
+
+ 		//replace the ?
+ 		$stmt->bind_param("i", $_GET["delete"]);
+
+ 		if($stmt->execute()){
+ 			echo "deleted successfully";
+ 		}else{
+ 			echo $stmt->error;
+ 		}
+
+ 		//closes the statement, so others can use connection
+ 		$stmt->close();
+
+ 	}
+
+
+
+
+
+
+ 	//SQL sentence
+ 	$stmt = $mysql->prepare("SELECT id, item_1, budget FROM Grocery_list WHERE deleted IS NULL ORDER BY created DESC LIMIT 10");
+
+ 	//WHERE deleted IS NULL show only those that are not deleted
+
+ 	//if error in sentence
+ 	echo $mysql->error;
+
+ 	//variables for data for each row we will get
+ 	$stmt->bind_result($id, $item_1, $budget);
+
+ 	//query
+ 	$stmt->execute();
+
+ 	$table_html = "";
+
+ 	//add smth to string .=
+ 	$table_html .= "<table class=table table-'>";
+ 		$table_html .= "<tr>";
+ 			$table_html .= "<th>ID</th>";
+ 			$table_html .= "<th>Your Item</th>";
+ 			$table_html .= "<th>How much</th>";
+ 			$table_html .= "<th>Delete ?</th>";
+ 		$table_html .= "</tr>";
+
+ 	// GET RESULT
+ 	//we have multiple rows
+ 	while($stmt->fetch()){
+
+ 		//DO SOMETHING FOR EACH ROW
+ 		//echo $id." ".$message."<br>";
+ 		$table_html .= "<tr>"; //start new row
+ 			$table_html .= "<td>".$id."</td>"; //add columns
+ 			$table_html .= "<td>".$item_1."</td>";
+ 			$table_html .= "<td>".$budget."</td>";
+ 			$table_html .= "<td>
+ 			<a class='btn btn-danger'
+ 								href='?delete=".$id."'>Away!</a>
+ 							</td>";
+
+
+
+ 		$table_html .= "</tr>"; //end row
+ 	}
+ 	$table_html .= "</table>";
+
+
+
+
+ ?>
